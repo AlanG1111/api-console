0e697fe157dec974e4d60cc08e3ea74aa426a41c
Alan Gasanov <alan@MacBook-Air-Alan.local>
2022-02-08 08:25:11 +0200

    fixed all colored buttons

--- src/containers/console_page/Footer.js
+++ src/containers/console_page/Footer.js
@@ -1,3 +1,4 @@
+import format from './icons/format';
 import React from "react";
 
 const Footer = () => {
@@ -5,9 +6,8 @@ const Footer = () => {
         <div className="console-footer">
             <button className="blue-button">Отправить</button>
             <span><a href="https://github.com/AlanG1111">@github.com/AlanG1111</a></span>
-            <div className="console-footer-format">
-                <img src="/icons/format.svg" alt="format" />
-                <span>Форматировать</span>
+            <div className='console-colored-button console-footer-format'>
+                {format} <span>Форматировать</span> 
             </div>
         </div>
     )
